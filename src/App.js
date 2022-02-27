7ae9f801af4a40bdc03dcee5e6450a95a8fa989c
Kush Pathak <you@example.com>
2022-02-27 12:47:29 +0530

    2nd/3rd day commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import Checkout from './Checkout';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
+      <Header />
+      <Routes>
+
+        <Route path="/" element={<><Home /></>} />
+        <Route path ="/checkout" element={<><Checkout /></>} />
       
-     <Header />
-      <Home />
+      </Routes>
     </div>
+    </Router>
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,20 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import {Link} from "react-router-dom";
+import { useStateValue } from './StateProvider';
 
 function Header() {
+
+  const [{basket},dispatch]= useStateValue();
+
   return <div className='header'>
+    <Link to="/">
      <img 
         className="header_logo"
         src={"./amazon_PNG25.png"}
     /> 
+    </Link>
     <div className="header_search">
 
     <input  
@@ -43,12 +50,14 @@ function Header() {
         Prime
       </span>
     </div>
+    <Link to="/checkout">
   <div className="header_optionBasket">
     <ShoppingBasketIcon />
     <span className="header_optionLineTwo header_basketCount">
-      0
+      {basket?.length}
     </span>
   </div>
+  </Link>
 
   </div>
   </div>;
